620da1f3a2721f942c7b3a8066d5568c7bdb2bf4
Alba Casas <albacasasarzua1b@gmail.com>
2022-01-22 19:03:58 +0100

    fix string to number

--- kyu7/square-every-digit/index.js
+++ kyu7/square-every-digit/index.js
@@ -1,6 +1,6 @@
 function squareDigits(num) {
   const list = (num + "").split("");
-  return list.map((n) => n ** 2);
+  return +list.map((n) => n ** 2).join("");
 }
 
 console.log(squareDigits(9119));
